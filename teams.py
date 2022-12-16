65b0ba1ee15a332da04a7ced5a4a9fc10473a375
Brian Breeden <bcbreeden@proton.me>
2022-12-16 11:30:23 -0500

    started team import of data

--- teams.py
+++ teams.py
@@ -6,7 +6,8 @@ DATA = pd.read_csv('static/data/data_teams.csv')
 Fetches team data from the CSV file via a list of team ids. The data is then returned in a list.
 '''
 def get_team_data_by_id(team_id):
-    team = DATA.loc[DATA['id']==team_id]
+    team = DATA.loc[DATA['id']==int(team_id)]
+    print(team.head())
     return(team)
 
 '''
