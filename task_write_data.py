8728ff51a9c913a04b69b727a229b80fc63045b7
Brian Breeden <bcbreeden@proton.me>
2023-02-13 20:59:54 -0500

    removed a huge block of code for Per Game Diffs, no longer used

--- task_write_data.py
+++ task_write_data.py
@@ -2,7 +2,7 @@ from api_players import write_player_data
 from api_schedule import write_game_schedule
 from api_teams import write_team_data
 from api_player_game_history import write_player_game_history_data
-from skaters import add_skater_averages_to_csv, set_hot_cold
+from skaters import set_hot_cold
 
 if __name__ == "__main__":
     print('Player write in progress...', flush=True)
@@ -16,9 +16,6 @@ if __name__ == "__main__":
 
     print('Writing player game history...', flush=True)
     write_player_game_history_data()
-
-   # print('Cleaning player data...', flush=True)
-   # add_skater_averages_to_csv()
 
     print('Who is hot, who is cold? Lets find out...', flush=True)
     set_hot_cold()
