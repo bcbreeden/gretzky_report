ce33cc4902750741c0fb1834423f16d659dea94c
Brian Breeden <bcbreeden@proton.me>
2023-02-20 18:52:34 -0500

    added goalie fantasy points to game history

--- api_player_game_history.py
+++ api_player_game_history.py
@@ -33,7 +33,7 @@ def get_player_game_history():
                 record['shots'] = game['stat']['shots']
                 record['PPTOI'] = game['stat']['powerPlayTimeOnIce']
                 record['blocks'] = game['stat']['blocked']
-                record['fantasy_points'] = get_game_fantasy_points(record['goals'], record['assists'], record['shots'], record['blocks'])
+                record['fantasy_points'] = get_skater_fantasy_points(record['goals'], record['assists'], record['shots'], record['blocks'])
                 player_game_history_skaters.append(record)
                 print('Skater record add for {} {}.'.format(player_id, record['date']), flush=True)
             except KeyError:
@@ -42,6 +42,17 @@ def get_player_game_history():
                     record['save_perc'] = game['stat']['savePercentage']
                     record['goals_against'] = game['stat']['goalsAgainst']
                     record['shots_against'] = game['stat']['shotsAgainst']
+                    record['decision'] = game['stat']['decision']
+                    record['shutout'] = game['stat']['shutouts']
+                    record['save_pctg'] = game['stat']['savePercentage']
+                    record['ot'] = game['stat']['ot']
+                    record['fantasy_points'] = get_goalie_fantasy_points(
+                        record['decision'],
+                        record['saves'],
+                        record['goals_against'],
+                        record['shutout'],
+                        record['ot']
+                    )
                     player_game_history_goalies.append(record)
                     print('Goalie record add for {}.'.format(player_id), flush=True)
                 except Exception:
@@ -58,10 +69,17 @@ def write_player_game_history_data():
     goalie_df.to_csv(get_data_path('data_game_history_goalie.csv'), encoding='utf-8', index=False)
     print('Goalie game history file written successfully!', flush=True)
 
-def get_game_fantasy_points(goals, assists, shots, blocks):
+def get_skater_fantasy_points(goals, assists, shots, blocks):
     score = (goals * 8.5) + (assists * 5) + (shots * 1.5) + (blocks * 1.3)
     score = (score + 3) if (goals >=3) else score #Hat Trick Bonus
     score = (score + 3) if (shots >=5) else score #Shooter Bonus
     score = (score + 3) if (blocks >=3) else score #Blocker Bonus
     score = (score + 3) if ((goals + assists) >= 3) else score #Points Bonus
+    return score
+
+def get_goalie_fantasy_points(decision, saves, goals_against, shutout, ot):
+    score = (saves * 0.7) + (goals_against * (-3.5))
+    score = (score + 6) if (decision == 'W') else score
+    score = (score + 4) if (shutout == 1) else score
+    score = (score + 2) if (ot == 1) else score
     return score
